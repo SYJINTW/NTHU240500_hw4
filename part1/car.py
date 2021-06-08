fcd2383804f616cccb677a49bd29060ff25a5610
SYJINTW <yuanjiun911@outlook.com>
2021-06-09 02:37:44 +0800

    add part1

---
+++ part1/car.py
@@ -0,0 +1,12 @@
+import time
+import serial
+import sys,tty,termios
+
+# XBee setting
+serdev = '/dev/ttyUSB0'
+s = serial.Serial(serdev, 9600, timeout=3)
+
+print('Start parking')
+s.write("/parking/run 5.0 3.0 west\n".encode())
+
+s.close()
